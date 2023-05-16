ed01ce25fa9c05a29f40fe877d15113fd5b508ef
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-05-16 10:52:15 +0200

    iprove challengeResponse schema

--- src/main/kotlin/cryptr/kotlin/models/ChallengeResponse.kt
+++ src/main/kotlin/cryptr/kotlin/models/ChallengeResponse.kt
@@ -8,17 +8,19 @@ data class ChallengeResponse(
     @SerialName("access_token") val accessToken: String?,
     @SerialName("id_token") val idToken: String?,
 
-    @SerialName("refresh_retry") val refreshRetry: String?,
-    @SerialName("refresh_leeway") val refreshLeeway: String?,
-    @SerialName("refresh_count") val refreshCount: String?,
+    @SerialName("refresh_retry") val refreshRetry: Int?,
+    @SerialName("refresh_leeway") val refreshLeeway: Int?,
+    @SerialName("refresh_count") val refreshCount: Int?,
 
     @SerialName("oauth_authorization_id") val oauthAuthorizationId: String?,
     @SerialName("resource_owner_id") val resourceOwnerId: String?,
     @SerialName("client_id") val clientId: String?,
     @SerialName("client_url") val clientUrl: String?,
-    @SerialName("nonce") val nonce: String?,
-    @SerialName("scope") val scope: String?,
-    @SerialName("assertion") val assertion: String?,
+    @SerialName("nonce") val nonce: Set<String>? = setOf(),
+    /** TEMP comment need to rollback asap **/
+//    @SerialName("nonce") val nonce: String?,
+    @SerialName("scope") val scope: Set<String>? = setOf(),
+    @SerialName("assertion") val assertion: Map<String, String> = mapOf<String, String>(),
     @SerialName("token_type") val tokenType: String?,
 
     @SerialName("expires_at") val expiresAt: String?,
