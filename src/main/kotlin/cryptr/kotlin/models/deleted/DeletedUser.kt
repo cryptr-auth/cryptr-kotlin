8bce0351193f0d163efd70f404e1e1d27ec63888
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-05-22 18:43:14 +0200

    documentation for Deleted resources

--- src/main/kotlin/cryptr/kotlin/models/deleted/DeletedUser.kt
+++ src/main/kotlin/cryptr/kotlin/models/deleted/DeletedUser.kt
@@ -6,12 +6,23 @@ import kotlinx.serialization.Required
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a deleted [User]
+ */
 @Serializable
 @SerialName("DeletedUser")
 data class DeletedUser(
+    /**
+     * @see DeletedResource.deleted
+     */
     @Required
     @SerialName("deleted")
     override val deleted: Boolean,
+
+    /**
+     * Represent the deleted [User]
+     * @see DeletedResource.resource
+     */
     @Required
     @SerialName("resource")
     override val resource: User
