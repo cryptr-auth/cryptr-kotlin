811990ad3828c5d6b85f0f41b90ad2395bae5c90
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-05-23 09:21:30 +0200

    add missing documentation

--- src/main/kotlin/cryptr/kotlin/models/ChallengeResponse.kt
+++ src/main/kotlin/cryptr/kotlin/models/ChallengeResponse.kt
@@ -5,34 +5,83 @@ import cryptr.kotlin.models.jwt.JWTToken
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a successful response of (SSO) Challenge Headless Authenticatino process
+ */
 @Serializable
 data class ChallengeResponse(
+    /**
+     * The JWT Access Token of the authenticated end-user
+     */
     @SerialName("access_token") val accessToken: String? = null,
+    /**
+     * The JWT ID Token of the authenticated end-user
+     */
     @SerialName("id_token") val idToken: String? = null,
 
     @SerialName("refresh_retry") val refreshRetry: Int?,
     @SerialName("refresh_leeway") val refreshLeeway: Int?,
+    /**
+     * Number of refresh made on these JWT tokens
+     */
     @SerialName("refresh_count") val refreshCount: Int?,
 
+    /**
+     * Associated oauth authorization unique identifier
+     */
     @SerialName("oauth_authorization_id") val oauthAuthorizationId: String?,
+    /**
+     * Unique identifier of the resource owner of these tokens
+     */
     @SerialName("resource_owner_id") val resourceOwnerId: String?,
+    /**
+     * Unique identifier of the Client ([Application])
+     */
     @SerialName("client_id") val clientId: String?,
+    /**
+     * URL of the client
+     */
     @SerialName("client_url") val clientUrl: String?,
     @SerialName("nonce") val nonce: Set<String>? = setOf(),
     /** TEMP comment need to rollback asap **/
 //    @SerialName("nonce") val nonce: String?,
+    /**
+     * Authorized scope of these generated tokens
+     */
     @SerialName("scope") val scope: Set<String>? = setOf(),
+    /**
+     * SSO assertions of these tokens
+     */
     @SerialName("assertion") val assertion: Map<String, String> = mapOf<String, String>(),
+    /**
+     * Type of token
+     */
     @SerialName("token_type") val tokenType: String?,
 
+    /**
+     * Expiration date as [String]
+     */
     @SerialName("expires_at") val expiresAt: String?,
+    /**
+     * Refresh expiration date (as [String])
+     */
     @SerialName("refresh_token_expires_at") val refreshTokenExpiresAt: String?,
 ) : Tokenable {
+    /**
+     * Decode ID Token and retrieve its [JWTToken] if its valid
+     *
+     * @see idToken
+     */
     fun getIdClaims(baseUrl: String): JWTToken? {
         val token: String = idToken.toString()
         return verify(baseUrl, token)
     }
 
+    /**
+     * Decode Access Token and retrieve its [JWTToken] if its valid
+     *
+     * @see accessToken
+     */
     fun getAccessClaims(baseUrl: String): JWTToken? {
         val token: String = accessToken.toString()
         return verify(baseUrl, token)
--- src/main/kotlin/cryptr/kotlin/models/Profile.kt
+++ src/main/kotlin/cryptr/kotlin/models/Profile.kt
@@ -3,20 +3,50 @@ package cryptr.kotlin.models
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Represent the identity of the [User]
+ */
 @Serializable
 data class Profile(
     @SerialName("__type__") override val cryptrType: String = "Profile",
     @SerialName("__environment__") override val environment: String? = null,
     @SerialName("__domain__") override val resourceDomain: String? = null,
+    /**
+     * Date of birth of the User (as [String])
+     */
     val birthdate: String? = null,
+    /**
+     * Family name of the User
+     */
     @SerialName("family_name") val familyName: String? = null,
+    /**
+     * Gender of the User. Ex: `male`or `female`
+     */
     val gender: String? = null,
+    /**
+     * Given name of the User
+     */
     @SerialName("given_name") val givenName: String? = null,
+    /**
+     * Preferred locale of User
+     */
     val locale: String? = null,
+    /**
+     * Nickname of the User
+     */
     val nickname: String? = null,
+    /**
+     * Picture of the User (ex Avatar)
+     */
     val picture: String? = null,
+    /**
+     * Preferred Username
+     */
     @SerialName("preferred_username") val preferredUsername: String? = null,
     val website: String? = null,
+    /**
+     * Geographical region of the User. Ex: `"Europe/Paris"`
+     */
     @SerialName("zoneinfo") val zoneInfo: String? = null
 ) : CryptrResource() {
     companion object {
