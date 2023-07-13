feb11fa2032107d17384c27c3f4197c60dc4b8fe
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-07-13 18:30:24 +0200

    improve renew password structure with password code + passwordChallenge functions

--- src/main/kotlin/cryptr/kotlin/models/PasswordChallenge.kt
+++ src/main/kotlin/cryptr/kotlin/models/PasswordChallenge.kt
@@ -4,12 +4,17 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+class RenewPassword(
+    @SerialName("password_code") val passwordCode: String
+) {}
+
+@Serializable
 class PasswordChallenge(
     @SerialName("__type__") override val cryptrType: String = "PasswordChallenge",
 
     @SerialName("error") val error: String? = null,
     @SerialName("expired_at") val expiredAt: String? = null,
-    @SerialName("renew_password") val renewPassword: String? = null,
+    @SerialName("renew_password") val renewPassword: RenewPassword? = null,
     @SerialName("request_id") val requestId: String,
     @SerialName("valid?") val isValid: Boolean = false,
     @SerialName("code") val code: String? = null,
@@ -18,5 +23,13 @@ class PasswordChallenge(
 
     fun isSuccess(): Boolean {
         return isValid;
+    }
+
+    fun isExpired(): Boolean {
+        return expiredAt !== null
+    }
+
+    fun getRenewCode(): String {
+        return renewPassword?.passwordCode!!
     }
 }
