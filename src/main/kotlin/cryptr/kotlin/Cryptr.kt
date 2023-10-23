387afa4f24e6c063153ae8d1ca1be78746dd6672
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-10-23 18:35:13 +0200

    define a standard challenge validation function

--- src/main/kotlin/cryptr/kotlin/Cryptr.kt
+++ src/main/kotlin/cryptr/kotlin/Cryptr.kt
@@ -465,6 +465,29 @@ class Cryptr(
         }
     }
 
+
+    /**
+     * Consumes the code value to retrieve authentication payload containing end-user JWTs
+     *
+     * @param code the query param received on your callback endpoint(redirectUri from create challenge fun)
+     * @return [ChallengeResponse] containing end-user session JWTs
+     */
+    fun validateChallenge(code: String? = null): APIResult<ChallengeResponse, ErrorMessage> {
+        if (code !== null && code.isNotEmpty() && code.isNotBlank()) {
+            val params = mapOf("code" to code, "grant_type" to "authorization_code")
+            val response =
+                makeRequest(path = "api/v2/oauth/token", serviceUrl, params, apiKeyToken = retrieveApiKeyToken())
+            return try {
+                APISuccess(format.decodeFromString<ChallengeResponse>(response.toString()))
+            } catch (e: Exception) {
+                logException(e)
+                APIError(ErrorMessage(response.toString()))
+            }
+        } else {
+            return APIError(ErrorMessage("code is required"))
+        }
+    }
+
     /**
      * Consumes the code value to retrieve authentication payload containing end-user JWTs
      *
@@ -483,7 +506,6 @@ class Cryptr(
                 apiKeyToken = retrieveApiKeyToken()
             )
             return try {
-                logDebug({ response.toString() })
                 APISuccess(format.decodeFromString<ChallengeResponse>(response.toString()))
             } catch (e: Exception) {
                 logException(e)
